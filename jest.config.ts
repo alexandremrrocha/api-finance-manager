4584766141ea0b3c5d9c0e63936629354fa54cd0
Alexandre Miranda Rezende Rocha <alexandremrr2014@gmail.com>
2025-09-27 16:57:47 -0300

    test: adicionado cobertura de teste com o limite minimo de 80% do codigo

--- jest.config.ts
+++ jest.config.ts
@@ -8,4 +8,12 @@ module.exports = {
   transform: {
     ...tsJestTransformCfg,
   },
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },    
+  },
 };
